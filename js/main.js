7b1412c97399a638016f73b7361c23055c851e53
schneiderjao <48598964+schneiderjao@users.noreply.github.com>
2020-10-07 20:07:49 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,18 +1,35 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    const url = '' //ADICIONAR
-    
-    
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET","https://cors-anywhere.herokuapp.com/%22+url");
-    xhr.setRequestHeader("chave-api-dados", "aa80352512269d345d61833ec008493b");
-    xhr.setRequestHeader("Accept", "/");
-    xhr.send(null);
-    
-    xhr.onreadystatechange = function() {
-        if(xhr.readyState === 4){
-            console.log(JSON.parse(xhr.responseText)
-            );
-        }
-    }
+    let pokemon = Math.floor(Math.random() * 151);
+    const url = "http://educacao.dadosabertosbr.com/api/escola/"; //identar com o inep da escola q o usuario digitar taokei
+    fetch(url)
+    .then(response => response.json())
+    .then(personagem => {
 
+        //document.querySelector(".nome").innerHTML  = personagem.name;
+        //document.querySelector('img').src= personagem.sprites['front_shiny'];
+    });
 });
+
+
+
+
+//NAO remover
+
+//document.addEventListener("DOMContentLoaded", function(event) {
+   // const url = '' //ADICIONAR
+    
+    
+   // var xhr = new XMLHttpRequest();
+  //  xhr.open("GET","https://cors-anywhere.herokuapp.com/%22+url");
+   // xhr.setRequestHeader("chave-api-dados", "aa80352512269d345d61833ec008493b");
+  //  xhr.setRequestHeader("Accept", "/");
+ //   xhr.send(null);
+    
+ //   xhr.onreadystatechange = function() {
+ //       if(xhr.readyState === 4){
+  //          console.log(JSON.parse(xhr.responseText)
+   //         );
+  //      }
+  //  }
+
+//});
