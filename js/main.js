9662779e4b449686dd6fec2121291a92d602bf22
schneiderjao <48598964+schneiderjao@users.noreply.github.com>
2020-10-07 11:29:17 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    let pokemon = Math.floor(Math.random() * 151);
-    const url = "https://pokeapi.co/api/v2/pokemon/"+pokemon;
-    fetch(url)
-    .then(response => response.json())
-    .then(personagem => {
-        document.querySelector(".nome").innerHTML  = personagem.name;
-        document.querySelector('img').src= personagem.sprites['front_default'];
-        if(personagem.types.length == 1 ){
-            document.querySelector('.type').innerHTML+= personagem.types[0]['type']['name'];
-        } else if(personagem.types.length == 2){
-            document.querySelector('.type').innerHTML+= personagem.types[0]['type']['name'] + " & " + personagem.types[1]['type']['name'];
+    const url = 'http://www.portaltransparencia.gov.br/api-de-dados/cartoes?mesExtratoInicio=11%2F2019&mesExtratoFim=10%2F2020&pagina=1'
+    
+    
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET","https://cors-anywhere.herokuapp.com/%22+url");
+    xhr.setRequestHeader("chave-api-dados", "aa80352512269d345d61833ec008493b");
+    xhr.setRequestHeader("Accept", "/");
+    xhr.send(null);
+    
+    xhr.onreadystatechange = function() {
+        if(xhr.readyState === 4){
+            console.log(JSON.parse(xhr.responseText)
+            );
         }
-    });
+    }
+
 });
